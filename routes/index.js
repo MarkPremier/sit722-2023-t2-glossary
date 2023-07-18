4a8968589a2a11d12359bd7561dffa95fad5ca96
MarkPremier <markpremier@gmail.com>
2023-07-18 20:08:23 +1000

    Changed layout

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,29 @@
 var express = require("express");
 var router = express.Router();
 
-const nameList = [
-  { id: 1, name: "John" },
-  { id: 2, name: "James" },
-  { id: 3, name: "Jessie" },
+const heading = [
+  {
+    id: "ID#",
+    term: "Term",
+    description: "Description",
+    references: "References",
+  },
+];
+
+const termList = [
+  { id: 1, term: "John", description: "Test", references: "Test" },
+  { id: 2, term: "James", description: "Test", references: "Test" },
+  { id: 3, term: "Jessie", description: "Test", references: "Test" },
 ];
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", {
-    title: "SIT722",
-    subTitle: "Deakin Uni",
-    names: nameList,
+    title: "SIT722 DevOps Glossary",
+    subTitle:
+      "Welcome to my glossary website, where I will share a collection of DevOps phrases I have learned over the course of this unit.",
+    terms: termList,
+    headings: heading,
   });
 });
 
